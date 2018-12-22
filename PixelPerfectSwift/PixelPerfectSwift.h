35f97e468c84f03a3d64378ade1e145354cccaaa
iPetez P <petezetep@gmail.com>
2018-12-22 19:26:10 +0100

    Initial Commit

---
+++ PixelPerfectSwift/PixelPerfectSwift.h
@@ -0,0 +1,19 @@
+//
+//  PixelPerfectSwift.h
+//  PixelPerfectSwift
+//
+//  Created by Zsolt Pete on 2018. 12. 22..
+//  Copyright © 2018. Zsolt Pete. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PixelPerfectSwift.
+FOUNDATION_EXPORT double PixelPerfectSwiftVersionNumber;
+
+//! Project version string for PixelPerfectSwift.
+FOUNDATION_EXPORT const unsigned char PixelPerfectSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PixelPerfectSwift/PublicHeader.h>
+
+
